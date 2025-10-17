9efbd5a61cbf3d5b53a64ac058c5538834bfe899
2300032527 <163747658+2300032527@users.noreply.github.com>
2025-10-18 01:37:36 +0530

    Update HospitalApplication.java

--- hospital-backend-jenkins/src/main/java/com/klu/hospital/HospitalApplication.java
+++ hospital-backend-jenkins/src/main/java/com/klu/hospital/HospitalApplication.java
@@ -1,11 +1,20 @@
 package com.klu.hospital;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootTest(classes = HospitalApplication.class)
-class HospitalApplicationTests {
+@SpringBootApplication
+public class HospitalApplication extends SpringBootServletInitializer {
 
-    @Test
-    void contextLoads() {}
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+        System.out.println("hello");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HospitalApplication.class);
+    }
 }
